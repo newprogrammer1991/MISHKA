27d6bf53ab2a8754695f679893b9455e0e9ab9df
Ali <ali.kholmatov@gmail.com>
2017-06-17 22:03:54 +0300

    added production

---
+++ build/js/googleMap.js
@@ -0,0 +1,27 @@
+"use strict";
+
+function initMap() {
+    var icons = {
+        main_address: {
+            icon: "../img/icons-svg/icon-map-pin.svg"
+        }
+    };
+    var uluru = { lat: 59.9387942, lng: 30.3244322802915 };
+    var map = new google.maps.Map(document.getElementById("map"), {
+        center: uluru,
+        scrollwheel: false,
+        zoom: 13
+    });
+
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+        icon: icons.main_address.icon
+    });
+
+    google.maps.event.addDomListener(window, "resize", function () {
+        var center = map.getCenter();
+        google.maps.event.trigger(map, "resize");
+        map.setCenter(center);
+    });
+}
